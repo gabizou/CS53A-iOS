e81f2e5c6d9d26beefe6c0371e1bf02080b45bb0
Gabriel Harris-Rouquette <gabizou@me.com>
2012-12-16 10:26:21 -0800

    Another Test with Xcode

---
+++ Test/Test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Test
+//
+//  Created by Gabriel Harris-Rouquette on 12/16/12.
+//  Copyright (c) 2012 Gabriel Harris-Rouquette. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
